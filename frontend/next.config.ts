497a4823c1c05ee64b6c4184715cdc906cff1800
Paras <parasmanhas013@gmail.com>
2026-06-28 23:44:53 +0530

    Fix proxy URL for production

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -19,10 +19,11 @@ const nextConfig: NextConfig = {
     ],
   },
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/api/:path*", // Proxy to Backend
+        destination: `${apiUrl}/api/:path*`, // Proxy to Backend
       },
     ];
   },
